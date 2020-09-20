26df47f55f1a85be1e3a0e1eaee06058fda2168c
htpdanh <70977838+htpdanh@users.noreply.github.com>
2020-09-20 11:25:41 +0300

    задание выполнил

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float pi = 3.14f;
+            float alpha;
+            Console.Write("Enter angle alpha : ");
+            alpha = float.Parse(Console.ReadLine());
+            while ((alpha <= 0) || (alpha >= 360))
+            {
+                Console.WriteLine("it's wrong, please try again with 0<alpha<360");
+                alpha = float.Parse(Console.ReadLine());
+            }
+            float x = (float)(alpha / 180) * pi;
+            Console.Write("{0} (degree) = {1} (radian)", alpha, x);
+            Console.ReadLine();
+        }
+    }
+}
